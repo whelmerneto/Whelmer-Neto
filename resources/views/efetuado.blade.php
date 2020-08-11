eb1250143925e50416e475fa95d11099920173fb
Whelmer <whelmer.neto@unioit.com>
2020-08-11 16:52:56 -0300

    crud finalizado

---
+++ resources/views/efetuado.blade.php
@@ -0,0 +1,102 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="/css/reset.css">
+
+		<title> Cadastro efetuado</title>
+
+	</head>
+
+	<body>
+
+		<div class="cabecalho">
+			<header> </header>
+		</div>
+
+		<div class="titulo"><h1> Cadastro efetuado com sucesso</h1> </div>
+
+		<div class="text"> 
+		 Para visualizar usuarios cadastrados
+		</div>
+
+		<div class="button">
+
+			<form action="usuarios">
+
+				<button type="submit" method="post"> clique aqui</button>
+
+			</form>
+		</div>
+
+			
+
+
+	</body>
+
+<style type="text/css">
+	
+	html {
+	font-family: 'Nunito', sans-serif;   
+	background-image: url(css/teste.jpeg);
+}
+
+body {
+	line-height: 1;
+}
+
+ol, ul {
+	list-style: none;
+}
+
+blockquote, q {
+	quotes: none;
+}
+
+.cabecalho {
+	height: 35px;
+	background-color: #000;
+}
+
+.titulo {
+	font-size: 60px;
+	text-align: center;
+	padding: 60px 0 80px 0;
+}
+
+.form {
+	font-weight: 600;
+	text-align: center;
+	position: relative;
+	left: 33%;
+	padding: 0 0 0 0;
+	border: 1px solid black;
+	height: 390px;
+ 	width:  450px;
+ 	border: solid;
+ 	border-radius: 8px;
+ 	background-color: #DCDCDC;
+
+}
+
+.nome, .sexo, .idade, .email, .celular {
+	padding: 42px 0 5px 0;
+}
+
+.text {
+	text-align: center;
+}
+
+button {
+	border:none;
+	color: white;
+	background-color: black;
+	border-radius: 10px;
+}
+
+.button {
+	padding: 35px 0 15px 0;
+	text-align: center;
+}
+</style>
+	</html>
